64ac9b59b2a396b0ba737db670384885fe7aec15
serhii <hokins296@gmail.com>
2024-09-01 12:51:46 +0300

    commit 1

--- h_t/h.py
+++ h_t/h.py
@@ -1,9 +1,23 @@
-class employee:
-    pass
+class Employee:
+    def __init__(self, full_name, role, telefon_number, email) -> None:
+        self.full_name = full_name
+        self.role = role
+        self.telefon_number = telefon_number
+        self.email = email
 
-class car:
-    pass
 
-class sells:
-    pass
+class Car:
+    def __init__(self, brand, manuf_year, model, price, potential_price) -> None:
+        self.brand = brand
+        self.manuf_year = manuf_year
+        self.model = model
+        self.cost = price
+        self.potential_cost = potential_price
 
+
+class Sells:
+    def __init__(self, employee: Employee, car: Car, date, real_price) -> None:
+        self.employee = employee
+        self.car = car
+        self.date = date
+        self.real_price = real_price
